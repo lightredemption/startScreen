4e7e869daab000b4acd37fc04bdb732d0bd86d61
lightredemption <lightredemption@hotmail.com>
2016-02-11 23:33:26 -0500

    last

--- js/main.js
+++ js/main.js
@@ -2,8 +2,16 @@ $(document).ready(function () {
   function startTime () {
     var today = new Date()
     $('#timer').html(today.toLocaleString())
-    $('#quote').html(quotes[Math.floor(Math.random() * (quotes.length))].toString())
+    getQuote()
     var t = setInterval(updateTime, 1000)
+  }
+
+  $('#quote').click(function (){
+    getQuote()
+  })
+
+  function getQuote () {
+    $('#quote').html(quotes[Math.floor(Math.random() * (quotes.length))].toString())
   }
 
   function updateTime () {
@@ -27,10 +35,16 @@ $(document).ready(function () {
 
   var quotes = [
     "Die hard, live harder",
+    "Doesn't hurt, doesn't count",
+    "There are two types of people: you and me",
+    "You always have time to not do something",
+    "Due tomorrow, do tomorrow",
+    "Fairness only in death",
+    "Equality is hating everyone equally",
     "Degenerates, all of you",
     "There's never a bus you have to chase after",
-    "All men are stupid. All women are crazy",
-    "There's plenty of time to sleep once you're dead",
+    "Men are stupid, women are crazy",
+    "You can sleep when you're dead",
     "Life is just convincing people you deserve things you don't"
   ]
   window.onload = startTime()
