aa2fc0efff10f2ccc7df11a8b6b79f6badb7870f
lightredemption <lightredemption@hotmail.com>
2016-06-19 08:18:00 -0400

    linted

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
   function startTime() {
-    var today = new Date()
-    $('#timer').html(today.toLocaleString())
-    window.i = Math.floor(Math.random() * (quotes.length))
-    getQuote(window.i)
-    getWeather()
-    var t = setInterval(updateTime, 1000)
-    var c = setInterval(getWeather, 60000)
+    var today = new Date();
+    $('#timer').html(today.toLocaleString());
+    window.i = Math.floor(Math.random() * (quotes.length));
+    getQuote(window.i);
+    getWeather();
+    var t = setInterval(updateTime, 1000);
+    var c = setInterval(getWeather, 60000);
   }
 
   $('#quote').click(function() {
     if (i < quotes.length) {
-      i++
+      i++;
     } else {
-      i = 0
+      i = 0;
     }
-    getQuote(i)
-  })
+    getQuote(i);
+  });
 
   function getWeather() {
     $.simpleWeather({
@@ -24,40 +24,40 @@ $(document).ready(function() {
       woeid: '',
       unit: 'c',
       success: function(weather) {
-        html = ' ' + weather.temp + '&deg;'
+        html = ' ' + weather.temp + '&deg;';
 
-        $('#temp').html(html)
+        $('#temp').html(html);
       },
       error: function(error) {
-        $('#temp').html('<p>' + error + '</p>')
+        $('#temp').html('<p>' + error + '</p>');
       }
-    })
+    });
   }
 
   function getQuote(index) {
-    $('#quote').html(quotes[index].toString())
+    $('#quote').html(quotes[index].toString());
   }
 
   function updateTime() {
-    var today = new Date()
-    $('#timer').html(today.toLocaleString())
+    var today = new Date();
+    $('#timer').html(today.toLocaleString());
   }
 
   $('#lock').click(function() {
-    $('#sfw').addClass('hidden')
-    $('#nsfw').removeClass('hidden')
-    $('#lock').addClass('hidden')
-    $('#unlock').removeClass('hidden')
-    $('#quote').removeClass('hidden')
-  })
+    $('#sfw').addClass('hidden');
+    $('#nsfw').removeClass('hidden');
+    $('#lock').addClass('hidden');
+    $('#unlock').removeClass('hidden');
+    $('#quote').removeClass('hidden');
+  });
 
   $('#unlock').click(function() {
-    $('#sfw').removeClass('hidden')
-    $('#nsfw').addClass('hidden')
-    $('#lock').removeClass('hidden')
-    $('#unlock').addClass('hidden')
-    $('#quote').addClass('hidden')
-  })
+    $('#sfw').removeClass('hidden');
+    $('#nsfw').addClass('hidden');
+    $('#lock').removeClass('hidden');
+    $('#unlock').addClass('hidden');
+    $('#quote').addClass('hidden');
+  });
 
   var quotes = [
     'Die hard, live harder',
@@ -89,7 +89,8 @@ $(document).ready(function() {
     'A perfect world is a totalitarian one',
     'Liberalism is about individual rights',
     'Collectivism is about undoing progress',
-    "Life isn't about living up to expectations of dead people"
-  ]
-  window.onload = startTime()
-})
+    "Life isn't about living up to expectations of dead people",
+    'Your fate is pre-determined, not by god, but by luck'
+  ];
+  window.onload = startTime();
+});
